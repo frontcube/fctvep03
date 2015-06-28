4386f8d725e2c8c17cb6c0f335176ce516074f68
Aslam Najeebdeen <aslam@frontcube.com>
2015-06-28 07:21:10 +0530

    Edited the header

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -28,6 +28,7 @@
 	<div id="sidebar" class="sidebar">
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
+			<h1>Edited</h1>
 				<?php
 					if ( is_front_page() && is_home() ) : ?>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
